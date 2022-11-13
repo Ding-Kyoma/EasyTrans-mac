4c81d1325201aa9d292b78156ec97ca5f55d2184
Ding-Kyoma <yiding.w@qq.com>
2022-11-13 18:55:36 -0500

    update

--- account.py
+++ account.py
@@ -1,4 +1,4 @@
 # 申请网站 http://api.fanyi.baidu.com/api/trans
 # 替换并加入更多的账号来分流预测， 用逗号隔开
-appid = ['20220404001158175']  # 20220404001158430', '20220405001158770', '20220405001159125', 
-secretKey = ['cRiWeFK2vi0JKRpsbsIY'] # 'Uyfcgu_hQY6mL4LOxqxL', 'l7utsO6PLFJa0tAwrAvt', 'J8S6mbYEfyY973idZPLV', 
+appid = ['20191210000364718']
+secretKey = ['e83BXpQFTnXrTy62O9MO']
